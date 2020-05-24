86c7cac6230bf9b31f14c381b5989e6beeabda2f
Jin-jin-jara <layharon@hanmail.net>
2020-05-24 16:31:31 +0900

    Create React x Node.js x MySQL project

--- src/App.js
+++ src/App.js
@@ -2,23 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// 실제 html의 <body> 태그에 해당하는 내용을 App.js가 채우게 된다.
+// 실질적인 웹 사이트 화면에 대한 내용 출력을 담당하는 부분
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="gray-background">
+        <img scr={logo} alt="logo" />
+        <h2>Let's develop management system</h2>
     </div>
   );
 }
